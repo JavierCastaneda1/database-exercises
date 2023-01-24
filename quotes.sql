cd3867425613d271c1f97c287c0f3a4e404d26fc
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-24 10:20:18 -0600

    lecture

--- quotes.sql
+++ quotes.sql
@@ -1,14 +1,14 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums (
-                        ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist VARCHAR(58),
-                        album_name VARCHAR(100),
-                        release_date INT,
-                        sales FLOAT,
-                        genre VARCHAR(50),
+CREATE TABLE quotes (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        author_first_name VARCHAR(50),
+                        author_last_name VARCHAR(100) NOT NULL,
+                        content TEXT NOT NULL,
                         PRIMARY KEY (id)
 );
+
+
 INSERT INTO quotes (author_first_name, author_last_name, content)
 VALUES ('Douglas', 'Adams',    'Time is an illusion. Lunchtime doubly so.'),
        ('Mark',    'Twain',    'Clothes make the man. Naked people have little or no influence on society.'),
