0e4f1e4f05f69853c441ee93e1b7f14df14cb94b
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-01-26 16:45:53 -0600

    completed exercise

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,7 +1,11 @@
 use employees;
+# Use the employees database.
 
-SELECT CONCAT(last_name, ' ', first_name) AS 'full_name' FROM employees;
+SELECT CONCAT(last_name, ' ', first_name) AS 'full_name' FROM employees LIMIT 25;
+# Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
 
-SELECT CONCAT(last_name,' ', first_name)AS 'full_name', birth_date AS 'DOB' FROM employees;
+SELECT CONCAT(last_name,' ', first_name)AS 'full_name', birth_date AS 'DOB' FROM employees LIMIT 25;
+# Add the date of birth for each employee as 'DOB' to the query.
 
-SELECT CONCAT(emp_no, ' - ',last_name,', ',first_name)AS 'full_name', birth_date AS 'DOB' FROM employees;
+SELECT CONCAT(emp_no, ' - ',last_name,', ',first_name)AS 'full_name', birth_date AS 'DOB' FROM employees LIMIT 25;
+# Update the query to format full name to include the employee number so it is formatted as 'employee number - last name, first name'.
